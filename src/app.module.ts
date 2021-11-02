8cb5a501870dbc82cfc6f3b054de62798b1cec2c
Luis Mario Ramírez Peralta <luismarioram99@gmail.com>
2021-11-02 16:26:27 -0600

    port configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TraceModule } from './trace/trace.module';
 
 @Module({
-  imports: [TraceModule],
+  imports: [TraceModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
